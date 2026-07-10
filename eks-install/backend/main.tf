7b03b500855712ec7475e3f6878489c6fa3d07b5
Ramesh Maddheshiya <ramjss08@gmail.com>
2026-07-11 04:04:34 +0530

    Update main.tf

--- eks-install/backend/main.tf
+++ eks-install/backend/main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "us-west-2"
+  region = "ap-south-1"
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "demo-terraform-eks-state-s3-bucket"
+  bucket = "ramesh-terraform-eks-state-271504343314"
 
   lifecycle {
     prevent_destroy = false
