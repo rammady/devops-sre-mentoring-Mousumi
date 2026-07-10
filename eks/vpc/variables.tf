65b157d6e7c866667e1194da732d6967e35e4e00
Ramesh Maddheshiya <ramjss08@gmail.com>
2026-07-11 02:40:55 +0530

    Update variables.tf

--- eks/vpc/variables.tf
+++ eks/vpc/variables.tf
@@ -1,32 +1,24 @@
-variable "cluster_name" {
-  description = "Name of the EKS cluster"
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
   type        = string
 }
 
-variable "cluster_version" {
-  description = "Kubernetes version"
-  type        = string
-}
-
-variable "vpc_id" {
-  description = "VPC ID"
-  type        = string
-}
-
-variable "subnet_ids" {
-  description = "Subnet IDs"
+variable "availability_zones" {
+  description = "Availability zones"
   type        = list(string)
 }
 
-variable "node_groups" {
-  description = "EKS node group configuration"
-  type = map(object({
-    instance_types = list(string)
-    capacity_type  = string
-    scaling_config = object({
-      desired_size = number
-      max_size     = number
-      min_size     = number
-    })
-  }))
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
 }
