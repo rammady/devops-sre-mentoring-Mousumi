cf3b8286b65e5677f493e13e2858729f15aa94c2
Ramesh Maddheshiya <ramjss08@gmail.com>
2026-07-12 03:06:44 +0530

    Create outputs.tf

---
+++ eks-install/outputs.tf
@@ -0,0 +1,19 @@
+output "cluster_endpoint" {
+  description = "EKS cluster API endpoint"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_name" {
+  description = "EKS cluster name"
+  value       = module.eks.cluster_name
+}
+
+output "vpc_id" {
+  description = "VPC ID"
+  value       = module.vpc.vpc_id
+}
+
+output "private_subnet_ids" {
+  description = "Private subnet IDs used by EKS"
+  value       = module.vpc.private_subnet_ids
+}
