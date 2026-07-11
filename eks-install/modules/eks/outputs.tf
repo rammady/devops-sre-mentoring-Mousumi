5aa4a9da3e56d732a77b49de5b8950fe92e1d354
Ramesh Maddheshiya <ramjss08@gmail.com>
2026-07-12 03:12:46 +0530

    Create outputs.tf

---
+++ eks-install/modules/eks/outputs.tf
@@ -0,0 +1,9 @@
+output "cluster_endpoint" {
+  description = "EKS cluster API endpoint"
+  value       = aws_eks_cluster.main.endpoint
+}
+
+output "cluster_name" {
+  description = "EKS cluster name"
+  value       = aws_eks_cluster.main.name
+}
